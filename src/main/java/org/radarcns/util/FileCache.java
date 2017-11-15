4fbb26919e82b6ea3150585061953b29689a6e5a
Joris Borgdorff <joris@thehyve.nl>
2017-11-15 11:37:18 +0100

    Removed stream array from filecache and improved error handling

--- src/main/java/org/radarcns/util/FileCache.java
+++ src/main/java/org/radarcns/util/FileCache.java
@@ -26,13 +26,17 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.zip.GZIPOutputStream;
 import javax.annotation.Nonnull;
 import org.apache.avro.generic.GenericRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Keeps path handles of a path. */
 public class FileCache implements Closeable, Flushable, Comparable<FileCache> {
-    private final OutputStream[] streams;
+    private static final Logger logger = LoggerFactory.getLogger(FileCache.class);
+
     private final Writer writer;
     private final RecordConverter recordConverter;
     private final Path path;
@@ -51,15 +55,25 @@ public class FileCache implements Closeable, Flushable, Comparable<FileCache> {
         this.path = path;
         boolean fileIsNew = !Files.exists(path) || Files.size(path) == 0;
 
-        this.streams = new OutputStream[gzip ? 3 : 2];
-        this.streams[0] = new FileOutputStream(path.toFile(), true);
-        this.streams[1] = new BufferedOutputStream(this.streams[0]);
+        OutputStream outFile = Files.newOutputStream(path,
+                StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+        OutputStream bufOut = new BufferedOutputStream(outFile);
         if (gzip) {
-            this.streams[2] = new GZIPOutputStream(this.streams[1]);
+            bufOut = new GZIPOutputStream(bufOut);
         }
 
-        this.writer = new OutputStreamWriter(this.streams[this.streams.length - 1]);
-        this.recordConverter = converterFactory.converterFor(writer, record, fileIsNew);
+        this.writer = new OutputStreamWriter(bufOut);
+
+        try {
+            this.recordConverter = converterFactory.converterFor(writer, record, fileIsNew);
+        } catch (IOException ex) {
+            try {
+                writer.close();
+            } catch (IOException exClose) {
+                logger.error("Failed to close writer for {}", path, ex);
+            }
+            throw ex;
+        }
     }
 
     /** Write a record to the cache. */
@@ -72,9 +86,6 @@ public class FileCache implements Closeable, Flushable, Comparable<FileCache> {
     public void close() throws IOException {
         recordConverter.close();
         writer.close();
-        for (int i = streams.length - 1; i >= 0; i--) {
-            streams[i].close();
-        }
     }
 
     @Override
