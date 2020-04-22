c03b2d2a1691cc01b76780bfff5be2429651c092
Joris Borgdorff <github@blootsvoets.net>
2020-04-22 09:42:30 +0200

    Added Azure storage drivers

---
+++ src/main/java/org/radarbase/output/kafka/AzureKafkaStorage.kt
@@ -0,0 +1,48 @@
+package org.radarbase.output.kafka
+
+import com.azure.storage.blob.BlobServiceClient
+import org.apache.avro.file.SeekableFileInput
+import org.apache.avro.file.SeekableInput
+import org.radarbase.output.util.TemporaryDirectory
+import org.radarbase.output.util.toKey
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+
+class AzureKafkaStorage(
+        client: BlobServiceClient,
+        container: String,
+        private val tempPath: Path
+): KafkaStorage {
+    private val blobContainerClient = client.getBlobContainerClient(container)
+
+    override fun list(path: Path): Sequence<SimpleFileStatus> = blobContainerClient.listBlobsByHierarchy("$path/")
+            .asSequence()
+            .map { SimpleFileStatus(Paths.get(it.name), it.isPrefix) }
+
+    override fun reader(): KafkaStorage.KafkaStorageReader = AzureKafkaStorageReader()
+
+    private inner class AzureKafkaStorageReader: KafkaStorage.KafkaStorageReader {
+        private val tempDir = TemporaryDirectory(tempPath, "worker-")
+
+        override fun newInput(file: TopicFile): SeekableInput {
+            val fileName = Files.createTempFile(tempDir.path, "${file.topic}-${file.path.fileName}", ".avro")
+
+            blobContainerClient
+                    .getBlobClient(file.path.toKey())
+                    .downloadToFile(fileName.toString())
+
+            return object : SeekableFileInput(fileName.toFile()) {
+                override fun close() {
+                    super.close()
+                    Files.deleteIfExists(fileName)
+                }
+            }
+        }
+
+        override fun close() {
+            tempDir.close()
+        }
+    }
+}
--- src/main/java/org/radarbase/output/kafka/KafkaStorageFactory.kt
+++ src/main/java/org/radarbase/output/kafka/KafkaStorageFactory.kt
@@ -1,5 +1,6 @@
 package org.radarbase.output.kafka
 
+import com.azure.storage.blob.BlobServiceClient
 import io.minio.MinioClient
 import org.apache.hadoop.fs.FileSystem
 import org.radarbase.output.config.ResourceConfig
@@ -9,6 +10,10 @@ import java.nio.file.Path
 class KafkaStorageFactory(private val resourceConfig: ResourceConfig, private val tempPath: Path) {
     private val s3SourceClient: MinioClient? = if (resourceConfig.sourceType == ResourceType.S3) {
         requireNotNull(resourceConfig.s3).createS3Client()
+    } else null
+
+    private val azureSourceClient: BlobServiceClient? = if (resourceConfig.sourceType == ResourceType.AZURE) {
+        requireNotNull(resourceConfig.azure).createAzureClient()
     } else null
 
     fun createKafkaStorage() = when(resourceConfig.sourceType) {
@@ -21,6 +26,11 @@ class KafkaStorageFactory(private val resourceConfig: ResourceConfig, private va
             val hdfsConfig = requireNotNull(resourceConfig.hdfs)
             val fileSystem = FileSystem.get(hdfsConfig.configuration)
             HdfsKafkaStorage(fileSystem)
+        }
+        ResourceType.AZURE -> {
+            val azureClient = requireNotNull(azureSourceClient)
+            val config = requireNotNull(resourceConfig.azure)
+            AzureKafkaStorage(azureClient, config.container, tempPath)
         }
         else -> throw IllegalStateException("Cannot create kafka storage for type ${resourceConfig.sourceType}")
     }
