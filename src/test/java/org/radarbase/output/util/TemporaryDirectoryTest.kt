44cb2670e81f0c71393d518399bce0b66859ba38
Joris Borgdorff <joris@thehyve.nl>
2020-09-10 12:24:14 +0200

    Fix temporary directory test

--- src/test/java/org/radarbase/output/util/TemporaryDirectoryTest.kt
+++ src/test/java/org/radarbase/output/util/TemporaryDirectoryTest.kt
@@ -2,9 +2,6 @@ package org.radarbase.output.util
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.equalTo
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Files
@@ -14,14 +11,14 @@ internal class TemporaryDirectoryTest {
     @Test
     fun createAndDelete(@TempDir root: Path) {
         TemporaryDirectory(root, "worker-").use {
-            assertThat(Files.list(root).count(), `is`(1))
+            assertThat(Files.list(root).count(), `is`(1L))
             Files.createTempFile(it.path, "test", "txt")
             Files.createTempFile(it.path, "test", "txt")
 
-            assertThat(Files.list(it.path).count(), `is`(2))
-            assertThat(Files.list(root).count(), `is`(1))
+            assertThat(Files.list(it.path).count(), `is`(2L))
+            assertThat(Files.list(root).count(), `is`(1L))
         }
 
-        assertThat(Files.list(root).count(), `is`(0))
+        assertThat(Files.list(root).count(), `is`(0L))
     }
 }
