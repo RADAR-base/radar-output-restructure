48ef650efc90bbd9fca6232820fa888cb88724a5
Bastiaan <bastiaan@thehyve.nl>
2024-03-27 12:21:30 +0100

    security updates and updated maintainers

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,13 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "2.3.3-SNAPSHOT"
+    const val project = "2.3.3"
 
     const val java = 17
-    const val kotlin = "1.9.10"
+    const val kotlin = "1.9.22"
     const val dockerCompose = "0.17.5"
 
-    const val radarCommons = "1.1.1"
-    const val radarSchemas = "0.8.5"
+    const val radarCommons = "1.1.2"
+    const val radarSchemas = "0.8.7"
     const val jackson = "2.15.3"
     const val slf4j = "2.0.9"
     const val log4j2 = "2.21.0"
@@ -19,12 +19,12 @@ object Versions {
 
     const val wrapper = "8.4"
 
-    const val managementPortal = "2.1.0"
+    const val managementPortal = "2.1.1"
     const val coroutines = "1.7.3"
     const val snappy = "1.1.10.5"
     const val jCommander = "1.82"
     const val almworks = "1.1.2"
-    const val minio = "8.5.6"
+    const val minio = "8.5.9"
     const val guava = "31.1-jre"
     const val opencsv = "5.8"
     const val okhttp = "4.12.0"
