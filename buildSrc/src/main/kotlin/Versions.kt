f2f835cbbde48afdc6313f7b1654684d92d54832
Pim van Nierop <pim@thehyve.nl>
2025-09-03 11:00:47 +0200

    Up project version to 3.0.4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "3.0.5"
+    const val project = "3.0.4"
 
     const val java = 17
     const val dockerCompose = "0.17.5"
